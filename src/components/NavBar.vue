9513e60f3dd276dd2dc06728311a98cc2af38f95
Mingsheng Gan <mingsheng88@hotmail.com>
2020-05-13 18:01:06 +0800

    Setup header

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">
+      <img alt="Vue logo" src="../assets/logo.png" height="30">
+      <b>
+        Appliances Store
+      </b>
+    </b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'nav-bar',
+  props: {}
+}
+</script>

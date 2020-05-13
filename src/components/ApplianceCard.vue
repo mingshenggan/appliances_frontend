30c607d13bd94712f233c812f3ab0282fd5dc878
Mingsheng Gan <mingsheng88@hotmail.com>
2020-05-14 02:24:49 +0800

    Setup ApplianceCard

---
+++ src/components/ApplianceCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <b-card
+    :data-id="appliance.id"
+    class="border">
+    <b-card-text>
+
+      <div class="row">
+        <label for="serialNo" class="col-sm-4 form-label">
+          S/N
+        </label>
+        <div class="col-sm-8">
+          <b-form-input v-model="appliance.id" plaintext readonly></b-form-input>
+        </div>
+      </div>
+
+      <div class="row">
+        <label for="serialNo" class="col-sm-4 form-label">
+          Brand
+        </label>
+        <div class="col-sm-8">
+          <b-form-input v-model="appliance.brand" plaintext readonly></b-form-input>
+        </div>
+      </div>
+
+      <div class="row">
+        <label for="serialNo" class="col-sm-4 form-label">
+          Model
+        </label>
+        <div class="col-sm-8">
+          <b-form-input v-model="appliance.model" plaintext readonly></b-form-input>
+        </div>
+      </div>
+
+      <div class="row">
+        <label for="serialNo" class="col-sm-4 form-label">
+          Status
+        </label>
+        <div class="col-sm-8">
+          <b-form-input v-model="appliance.status" plaintext readonly></b-form-input>
+        </div>
+      </div>
+
+      <div class="row">
+        <label for="serialNo" class="col-sm-4 form-label">
+          Date of Purchase
+        </label>
+        <div class="col-sm-8">
+          <b-form-input v-model="appliance.purchaseDate" plaintext readonly></b-form-input>
+        </div>
+      </div>
+    </b-card-text>
+  </b-card>
+</template>
+
+<script>
+  export default {
+    name: 'ApplianceCard',
+    props: {
+      appliance: { type: Object },
+    }
+  }
+</script>
--- src/views/AppliancesIndex.vue
+++ src/views/AppliancesIndex.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <h1>Hello World</h1>
-    {{ this.appliances }}
+    <div v-if='appliances.length > 0'>
+      <ApplianceCard
+        v-for='(appliance, i) in appliances'
+        :item='appliance'
+        :key='appliance.id'
+        :index='i'
+        :appliance='appliance'
+        class="mb-2"
+        />
+    </div>
+    <div class='d-flex w-100 h-20 justify-content-center' v-else>
+      No appliances found
+    </div>
   </div>
 </template>
 
 <script>
 import ApiService from "@/services/ApiService"
+import ApplianceCard from "@/components/ApplianceCard"
 import _ from "lodash"
 
 export default {
@@ -17,14 +30,14 @@ export default {
     }
   },
   created () { this.fetch_appliances() },
-  components: {},
+  components: {
+    ApplianceCard,
+  },
   methods: {
     fetch_appliances: _.debounce(
       function () {
         ApiService.fetchAppliances()
-          .then((response) => {
-            console.log(response)
-            this.appliances = response.data })
+          .then((response) => { this.appliances = response.data })
           .catch((error) => { alert(error) })
       }, 200),
   },

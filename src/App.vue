9aade98d494040d9ef771b5611132ed6bd1b3622
Mingsheng Gan <mingsheng88@hotmail.com>
2020-05-14 00:46:51 +0800

    Setup vue-router and extract view

--- src/App.vue
+++ src/App.vue
@@ -3,48 +3,23 @@
 
     <nav-bar></nav-bar>
 
-    <appliances-list
-      :appliances="appliances"
-      class="p-3">
-    </appliances-list>
+    <router-view></router-view>
 
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar'
-import AppliancesList from '@/components/AppliancesList'
-import axios from 'axios'
-import _ from 'lodash'
 
 export default {
   name: 'App',
   data () {
     return {
       loading: false,
-      appliances: [],
     }
   },
-  created () { this.fetch_appliances() },
   components: {
     'nav-bar': NavBar,
-    'appliances-list': AppliancesList,
-  },
-  methods: {
-    fetch_appliances: _.debounce(
-      function () {
-        this.loading = true
-        axios
-          .get(`https://appliances-spring.herokuapp.com/api/v1/appliances`)
-          .then((response) => {
-            this.appliances = response.data
-            this.loading = false;
-          })
-          .catch((error) => {
-            this.$emit('error_emitted', error)
-            this.loading = false;
-          })
-      }, 200),
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import AppliancesView from '@/views/AppliancesView.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,7 +11,18 @@ Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueRouter)
+
+let router = new VueRouter({
+  base: '/appliances_frontend/',
+  routes: [
+    { path: '/', name: 'appliances#index', component: AppliancesView },
+    // { path: '/appliances/:id', name: 'appliances#show', component: AppliancesShowView }
+  ],
+  mode: 'history'
+})
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')

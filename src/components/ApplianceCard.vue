fe795ab55b065bcfbf20b73493c05cb9b05f2565
Mingsheng Gan <mingsheng88@hotmail.com>
2020-05-14 02:41:04 +0800

    Add icons for edit and delete

--- src/components/ApplianceCard.vue
+++ src/components/ApplianceCard.vue
@@ -4,48 +4,57 @@
     class="border">
     <b-card-text>
 
-      <div class="row">
+      <div class="float-right">
+        <a href="" class="edit-button">
+          <b-icon-pencil class="mr-2" />
+        </a>
+        <button type="button" class="close" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>
+
+      <div class="row pb-2">
         <label for="serialNo" class="col-sm-4 form-label">
           S/N
         </label>
         <div class="col-sm-8">
-          <b-form-input v-model="appliance.id" plaintext readonly></b-form-input>
+          <b-form-input v-model="appliance.id" :plaintext='!editable' :readonly='!editable' />
         </div>
       </div>
 
-      <div class="row">
+      <div class="row pb-2">
         <label for="serialNo" class="col-sm-4 form-label">
           Brand
         </label>
         <div class="col-sm-8">
-          <b-form-input v-model="appliance.brand" plaintext readonly></b-form-input>
+          <b-form-input v-model="appliance.brand" :plaintext='!editable' :readonly='!editable' />
         </div>
       </div>
 
-      <div class="row">
+      <div class="row pb-2">
         <label for="serialNo" class="col-sm-4 form-label">
           Model
         </label>
         <div class="col-sm-8">
-          <b-form-input v-model="appliance.model" plaintext readonly></b-form-input>
+          <b-form-input v-model="appliance.model" :plaintext='!editable' :readonly='!editable' />
         </div>
       </div>
 
-      <div class="row">
+      <div class="row pb-2">
         <label for="serialNo" class="col-sm-4 form-label">
           Status
         </label>
         <div class="col-sm-8">
-          <b-form-input v-model="appliance.status" plaintext readonly></b-form-input>
+          <b-form-input v-model="appliance.status" :plaintext='!editable' :readonly='!editable' />
         </div>
       </div>
 
-      <div class="row">
+      <div class="row pb-2">
         <label for="serialNo" class="col-sm-4 form-label">
           Date of Purchase
         </label>
         <div class="col-sm-8">
-          <b-form-input v-model="appliance.purchaseDate" plaintext readonly></b-form-input>
+          <b-form-input v-model="appliance.purchaseDate" :plaintext='!editable' :readonly='!editable' />
         </div>
       </div>
     </b-card-text>
@@ -57,6 +66,18 @@
     name: 'ApplianceCard',
     props: {
       appliance: { type: Object },
+      editable: { type: Boolean, default: false },
     }
   }
 </script>
+
+<style lang="scss">
+  .edit-button {
+    color: inherit;
+    opacity: 0.6;
+
+    &:hover {
+      opacity: 1 !important;
+    }
+  }
+</style>
